7f88229993a1505fa3e561f7a19529f01f348bbc
Ahmed Maazouzi <ahmaazouzi@mix.wvu.edu>
2016-06-02 05:55:07 -0400

    added 1.17

---
+++ 1/1_17.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+#define MAXLINE 1000
+#define MAX 80
+
+int get_line(char line[], int maxline);
+void copy(char to[], char from[]);
+
+int main(){
+  int len;
+  char line[MAXLINE];
+  char longer[MAXLINE];
+
+
+  while((len = get_line(line, MAXLINE)) > 0)
+    if (len >= MAX){
+      copy(longer, line);
+      printf("%s\n", longer);
+    }
+  return 0;
+}
+
+int get_line(char s[], int lim){
+  int c, i;
+
+  for (i=0; i<lim-1 && (c=getchar())!=EOF && c!='\n'; ++i)
+    s[i] = c;
+  if (c == '\n'){
+    s[i] = c;
+    ++i;
+  }
+  s[i] = '\0';
+  return i;
+}
+
+void copy(char to[], char from[]){
+  int i;
+
+  i = 0;
+  while ((to[i] = from[i]) != '\0')
+    ++i;
+}
+

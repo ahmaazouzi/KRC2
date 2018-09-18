1a764fef117d5adde0d5587eb41b8d212ab7249b
Ahmed Maazouzi <ahmaazouzi@mix.wvu.edu>
2018-09-17 20:15:30 -0400

    1.16 was kinda tough.

--- ch1/1_16.c
+++ ch1/1_16.c
@@ -1,43 +1,66 @@
-/*
-This program prints lines and their lengths
-*/ 
-#include <stdio.h>
-#define MAXLINE 1000
+#include "stdio.h"
+#define MAXLINE 3
 
 int get_line(char line[], int maxline);
 void copy(char to[], char from[]);
 
 int main(){
-  int len;
-  char line[MAXLINE];
-  char longer[MAXLINE];
+	int len, i, c;
+	int max, maxmax;
+	char line[MAXLINE];
+	char longest[MAXLINE];
 
+	maxmax = 0;
+	max = 0;
+	while ((len = get_line(line, MAXLINE)) > 0){
+		if (len == (MAXLINE - 1)){
+			maxmax = len;
+			getchar();
+			maxmax = maxmax + 2;
+			while ((c = getchar()) != EOF && c != '\n'){
+				++maxmax;
+			}
+			copy(longest, line);
+		}
 
-  while((len = get_line(line, MAXLINE)) > 0)
-    if (len >= 0){
-      copy(longer, line);
-      printf("%d: %s", len, longer);
-    }
-  return 0;
+		if (len > max){
+			max = len;
+			copy(longest, line);
+
+		}
+	}
+	
+	if (maxmax > 0){
+		printf("%d\n", maxmax);
+		printf("%s\n", longest);		
+	} else if (max > 0){
+		printf("%d\n", max);
+		printf("%s\n", longest);		
+	}
+
+	return 0;
 }
 
 int get_line(char s[], int lim){
-  int c, i;
+	int c, i;
 
-  for (i=0; i<lim-1 && (c=getchar())!=EOF && c!='\n'; ++i)
-    s[i] = c;
-  if (c == '\n'){
-    s[i] = c;
-    ++i;
-  }
-  s[i] = '\0';
-  return i;
+	for (i=0; i < lim-1 && (c = getchar()) != EOF && c != '\n'; ++i){
+		s[i] = c;
+	}
+
+	if (c == '\n'){
+		s[i] = c;
+		++i;
+	}
+	s[i] = '\0';
+
+	return i;
 }
 
 void copy(char to[], char from[]){
-  int i;
+	int i;
 
-  i = 0;
-  while ((to[i] = from[i]) != '\0')
-    ++i;
+	i = 0;
+	while ((to[i] = from[i]) != '\0')
+		++i;
 }

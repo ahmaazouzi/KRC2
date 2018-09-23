fc5380eb69704d46322ce886e56f4cfb2554e098
Ahmed Maazouzi <ahmaazouzi@mix.wvu.edu>
2018-09-23 01:31:16 -0400

    Line fold should not start with a blank

--- ch1/1_22.c
+++ ch1/1_22.c
@@ -1,22 +1,26 @@
 #include "stdio.h"
-#define MAXLINESIZE 45
-#define MAXWORDSIZE 45
+#define MAXLINESIZE  45
+#define MAXWORDSIZE  45
+#define BEFORELINEEND 1
+#define AFTERLINEEND  0
 
 int main(){
-	int c, wordsize, lineLength, state;
+	int c, wordsize, lineLength, LineEndState;
 	char word[MAXWORDSIZE];
 	
+	LineEndState = BEFORELINEEND;
 	wordsize = 0;
 	lineLength = 0;
-
 	while ((c = getchar()) != EOF){
 		if (c ==  '\n'){
 			lineLength = 0;
+			LineEndState = AFTERLINEEND;
 		}
 		if (c == ' ' || c == '\t'){
 			if (lineLength == MAXLINESIZE) {
 				c = '\n';
 				lineLength = 0;
+				LineEndState = AFTERLINEEND;
 			}
 			else if ( wordsize > 0){
 				if (lineLength > MAXLINESIZE){
@@ -27,13 +31,20 @@ int main(){
 					putchar(word[i]);
 				}
 			}
+			if (LineEndState == AFTERLINEEND){
+				c = '\0';
+				--lineLength;
+			}
 			putchar(c);
 			wordsize = 0;
 		} else {
+			if (LineEndState == AFTERLINEEND){
+				LineEndState = BEFORELINEEND;
+				putchar('\n');
+			}
 			word[wordsize] = c;
 			++wordsize;
 		}
-
 		++lineLength;	
 	}
 }

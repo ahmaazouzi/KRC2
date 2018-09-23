e59a4a97cf492d3a0eff3946ecdac26e0c787398
Ahmed Maazouzi <ahmaazouzi@mix.wvu.edu>
2018-09-23 00:47:34 -0400

    Line folded correctly in blanks and otherwise

--- ch1/1_22.c
+++ ch1/1_22.c
@@ -1,27 +1,27 @@
 #include "stdio.h"
 #define MAXLINESIZE 45
 #define MAXWORDSIZE 45
-#define LESSTHANMAX 0
-#define MORETHANMAX 1
 
 int main(){
 	int c, wordsize, lineLength, state;
-
 	char word[MAXWORDSIZE];
+	
 	wordsize = 0;
-	state = LESSTHANMAX;
-
-
 	lineLength = 0;
+
 	while ((c = getchar()) != EOF){
-		if (c == ' ' || c == '\t' || c == '\n'){
-			if (lineLength >= MAXLINESIZE){
+		if (c ==  '\n'){
+			lineLength = 0;
+		}
+		if (c == ' ' || c == '\t'){
+			if (lineLength == MAXLINESIZE) {
 				c = '\n';
-				// lineLength = 0;
+				lineLength = 0;
 			}
-			if (wordsize > 0){
-				if (state == MORETHANMAX){
+			else if ( wordsize > 0){
+				if (lineLength > MAXLINESIZE){
 					putchar('\n');
+					lineLength = wordsize;
 				}
 				for (int i = 0; i < wordsize; ++i){
 					putchar(word[i]);
@@ -30,16 +30,8 @@ int main(){
 			putchar(c);
 			wordsize = 0;
 		} else {
-			if (lineLength >= MAXLINESIZE){
-				state = MORETHANMAX;
-				lineLength = 0;
-			}
 			word[wordsize] = c;
 			++wordsize;
-		}
-
-		if (lineLength < MAXLINESIZE){
-			state = LESSTHANMAX;
 		}
 
 		++lineLength;	

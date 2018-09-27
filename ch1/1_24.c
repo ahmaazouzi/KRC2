ab7c98fa9511753201b7cc4b0d2674dfa6d73bd4
Ahmed Maazouzi <ahmaazouzi@mix.wvu.edu>
2018-09-27 04:13:16 -0400

    Attempts at more modularity

--- ch1/1_24.c
+++ ch1/1_24.c
@@ -2,98 +2,105 @@
 
 #include "stdio.h"
 #include "stdlib.h" /* Not seen yet, but the concept is obvious*/
-#define MAXSYMBS		 1000
-#define OPENPARANTHESES	 '('
+#define MAXSYMBS         1000
+#define OPENPARANTHESES  '('
 #define CLOSEPARANTHESES ')'
-#define OPENBRACKET		 '['
-#define CLOSBRACKET		 ']'
-#define OPENBRACE		 '{'
-#define CLOSBRACE 		 '}'
-#define SINGLEQUOTE		 '\''
-#define DOUBLEQUOTE		 '"'
-#define BSLASHED		 1
-#define UNBSLASHED		 0
-#define BSLASH 			 '\\'
-#define LINEEND			 '\n'
-#define SLASH			 '/'
-#define STAR			 '*'
-#define INQUOTE			 1
-#define OUTQUOTE		 0
-#define INCOMMENT		 1
-#define OUTCOMMENT		 0
+#define OPENBRACKET      '['
+#define CLOSBRACKET      ']'
+#define OPENBRACE        '{'
+#define CLOSBRACE        '}'
+#define SINGLEQUOTE      '\''
+#define DOUBLEQUOTE      '"'
+#define BSLASHED         1
+#define UNBSLASHED       0
+#define BSLASH           '\\'
+#define LINEEND          '\n'
+#define SLASH            '/'
+#define STAR             '*'
+#define INQUOTE          1
+#define OUTQUOTE         0
+#define INCOMMENT        1
+#define OUTCOMMENT       0
+#define EMPTY            '\0'
 
 void getBalanceScore(char s[], int i);
-int deStringify();
-int unComment();
-int handleEscape();
+int deStringify(int c);
+// int unComment();
+// int handleEscape();
 
 int main(){
-	int c, i, slashState, commentState;
-	char groupers[MAXSYMBS];
+    int c, i, bSlashState, commentState, quoteState;
+    char groupers[MAXSYMBS];
+    // slash by itslef is a syntax error. come back to this
 
-	slashState = UNBSLASHED;
-	commentState = OUTCOMMENT;
-	i = 0;
-	while ((c = getchar()) != EOF){
-		if (c == BSLASH)
-			slashState = BSLASHED
-		else
-			slashState = UNBSLASHED;
+    bSlashState = UNBSLASHED;
+    commentState = OUTCOMMENT;
+    quoteState = OUTQUOTE;
+    i = 0;
+    while ((c = getchar()) != EOF){
 
-		c = deStringify(c, slashState);
-		c = unComment(c, commentState)
+        c = deStringify(c);
+        // c = unComment(c, commentState)
 
-		if (c == OPENPARANTHESES||c == CLOSEPARANTHESES||
-			c == OPENBRACE ||c == CLOSBRACE||
-			c == OPENBRACKET||c == CLOSBRACKET){
-
-			groupers[i] = c;
-			++i;
-		}		
-	}
-	++i;
-	groupers[i] = '\0';
-	getBalanceScore(groupers, i);
-	return 0;
+        if (c == OPENPARANTHESES||c == CLOSEPARANTHESES||
+            c == OPENBRACE ||c == CLOSBRACE||
+            c == OPENBRACKET||c == CLOSBRACKET){
+            groupers[i] = c;
+            ++i;
+        }  
+    }
+    ++i;
+    groupers[i] = EMPTY;
+    getBalanceScore(groupers, i);
+    return 0;
 }
 
 void getBalanceScore(char groupers[], int i){
-	int parantheses, braces, brackets;
-	brackets = 0;
-	braces = 0;
-	parantheses = 0;
-	for (int t = 0; t <= i; ++t){
-		if (groupers[t] == OPENPARANTHESES)
-			++parantheses;
-		if (groupers[t] == OPENBRACKET)
-			++brackets;
-		if (groupers[t] == OPENBRACE)
-			++braces;
-		if (groupers[t] == CLOSEPARANTHESES)
-			--parantheses;
-		if (groupers[t] == CLOSBRACE)
-			--braces;
-		if (groupers[t] == CLOSBRACKET)
-			--brackets;
-	}
+    int parantheses, braces, brackets;
+    brackets = 0;
+    braces = 0;
+    parantheses = 0;
+    for (int t = 0; t <= i; ++t){
+        if (groupers[t] == OPENPARANTHESES)
+            ++parantheses;
+        if (groupers[t] == OPENBRACKET)
+            ++brackets;
+        if (groupers[t] == OPENBRACE)
+            ++braces;
+        if (groupers[t] == CLOSEPARANTHESES)
+            --parantheses;
+        if (groupers[t] == CLOSBRACE)
+            --braces;
+        if (groupers[t] == CLOSBRACKET)
+            --brackets;
+    }
 
-	if (parantheses > 0)
-		printf("%d missing '%c'\n", parantheses, OPENPARANTHESES);
-	if (parantheses < 0)
-		printf("%d unexpexted '%c'\n", abs(parantheses), CLOSEPARANTHESES);
-	if (brackets < 0)
-		printf("%d missing '%c'\n", brackets, OPENBRACKET);
-	if (brackets > 0)
-		printf("%d unexpexted '%c'\n", abs(brackets), CLOSBRACKET);
-	if (braces < 0)
-		printf("%d missing '%c'\n", braces, OPENBRACE);
-	if (braces > 0)
-		printf("%d unexpexted '%c'\n", abs(braces), CLOSBRACE);
+    if (parantheses > 0)
+        printf("%d missing '%c'\n", parantheses, OPENPARANTHESES);
+    if (parantheses < 0)
+        printf("%d unexpexted '%c'\n", abs(parantheses), CLOSEPARANTHESES);
+    if (brackets < 0)
+        printf("%d missing '%c'\n", brackets, OPENBRACKET);
+    if (brackets > 0)
+        printf("%d unexpexted '%c'\n", abs(brackets), CLOSBRACKET);
+    if (braces < 0)
+        printf("%d missing '%c'\n", braces, OPENBRACE);
+    if (braces > 0)
+        printf("%d unexpexted '%c'\n", abs(braces), CLOSBRACE);
 }
 
-// int deStringify(){
-
-// }
+int deStringify(int c){
+    int quoteState;
+    quoteState = INQUOTE;
+    // if (c == DOUBLEQUOTE && quoteState == OUTQUOTE)
+    //     quoteState = INQUOTE;
+    // if (c == DOUBLEQUOTE && quoteState == INQUOTE)
+    //         quoteState = OUTQUOTE;
+    if (quoteState == INQUOTE)
+        c = '2'; 
+    printf("%c\n", c);
+    return c;
+}
 
 // // int unComment(){
 

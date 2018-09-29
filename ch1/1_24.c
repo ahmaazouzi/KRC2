46e55ceb576beb77af38f5564e8c8b103590601e
Ahmed Maazouzi <ahmaazouzi@mix.wvu.edu>
2018-09-29 03:04:32 -0400

    Does the trick '[(])' is an error

--- ch1/1_24.c
+++ ch1/1_24.c
@@ -1,4 +1,7 @@
-/* Check unbalanced parantheses, blanks, braces, quotes.. and comments */
+/* Check unbalanced parantheses, blanks, braces..
+ * If anything is inside single or double quote, comments, it's ignored.
+ * This could've been done recursively much more elegantly, but I work with what I have so far
+ */
 
 #include "stdio.h"
 #include "stdlib.h" /* Not seen yet, but the concept is obvious*/
@@ -21,35 +24,41 @@
 #define UNSTARRED        0
 #define INQUOTE          1
 #define OUTQUOTE         0
+#define INSQUOTE         1
+#define OUTSQUOTE        0
 #define INCOMMENT        1
 #define OUTCOMMENT       0
 #define EMPTY            '\0'
 
 void getBalanceScore(char s[], int i);
+void getUnbalanced(char groupers[], int i);
 int isInQuote(int c, int quoteCommentState);
 int isInComment(int c, int commentState, int slashState, int starState);
+int isInSQuote(int c, int sQuoteState);
 
 int main(){
-    int c, i, commentState, quoteState, slashState, starState;
+    int c, i, commentState, quoteState, slashState, starState, sQuoteState;
     char groupers[MAXSYMBS];
-    // slash by itslef is a syntax error. come back to this
+
 
     commentState = OUTCOMMENT;
     quoteState = OUTQUOTE;
     slashState = UNSLASHED;
+    sQuoteState = OUTSQUOTE;
     i = 0;
     while ((c = getchar()) != EOF){
-        quoteState = isInQuote(c, quoteState);
-        commentState = isInComment(c, commentState, slashState, starState);
+        // quoteState = isInQuote(c, quoteState);
+        // commentState = isInComment(c, commentState, slashState, starState);
+        // sQuoteState = isInSQuote(c, sQuoteState);
 
-        if (quoteState == OUTQUOTE && commentState == OUTCOMMENT){
+        // if (quoteState == OUTQUOTE && commentState == OUTCOMMENT && sQuoteState == OUTSQUOTE){
             if (c == OPENPARANTHESES||c == CLOSEPARANTHESES||
                 c == OPENBRACE ||c == CLOSBRACE||
                 c == OPENBRACKET||c == CLOSBRACKET){
                 groupers[i] = c;
                 ++i;
             } 
-        } 
+        // } 
 
         if (c == SLASH)
             slashState = SLASHED;
@@ -64,8 +73,65 @@ int main(){
     }
     ++i;
     groupers[i] = EMPTY;
-    getBalanceScore(groupers, i);
+
+    // getBalanceScore(groupers, i);
+    getUnbalanced(groupers, i);
+
     return 0;
+}
+
+void getUnbalanced(char groupers[], int i){
+    int s, sym;
+    char arrayHead;
+    char symbArray[MAXSYMBS];
+    arrayHead = '\0';
+    sym = 0 ;
+    for (s = 0; s <= i; ++s){
+        if (groupers[s] == OPENPARANTHESES || groupers[s] == OPENBRACKET ||
+        groupers[s] == OPENBRACE){
+            symbArray[sym] = groupers[s];
+            arrayHead = groupers[s];
+            ++sym;
+        }
+
+        if (groupers[s] == CLOSEPARANTHESES){
+            if (symbArray[sym - 1] == OPENPARANTHESES && sym > 0){
+                --sym;
+            }
+            else if (sym > 0){
+                printf("unexpectted %c\n", groupers[s]);
+                if (sym > 0)
+                    --sym;    
+            }
+        }
+
+        if (groupers[s] == CLOSBRACKET){
+            if (symbArray[sym - 1] == OPENBRACKET && sym > 0){
+                --sym;
+            }
+            else {
+                printf("unexpectted %c\n", groupers[s]);
+                if (sym > 0)
+                    --sym;    
+            }
+        }
+
+        if (groupers[s] == CLOSBRACE){
+            if (symbArray[sym - 1] == OPENBRACE && sym > 0){
+                --sym;
+            }
+            else {
+                printf("unexpectted %c\n", groupers[s]);
+                if (sym > 0)
+                    --sym;  
+            }
+        }
+    }
+
+    if (sym > 0){
+        for (s = 0; s < sym; ++s)
+            printf("A symbol corresponding to %c is missing.\n", symbArray[s]);
+    }
 }
 
 void getBalanceScore(char groupers[], int i){
@@ -120,7 +186,7 @@ int isInQuote(int c, int quoteState){
             quoteState = OUTQUOTE;
         }
     }
-    
+
     return quoteState;
 }
 
@@ -138,5 +204,15 @@ int isInComment(int c, int commentState, int slashState, int starState){
     }
 
     return commentState;
+}
+
+int isInSQuote(int c, int sQuoteState){
+    if (c == SINGLEQUOTE && sQuoteState == OUTSQUOTE)
+        sQuoteState = INSQUOTE;
+
+    else if (c == SINGLEQUOTE && sQuoteState == INSQUOTE)
+        sQuoteState = OUTSQUOTE;
+
+    return sQuoteState;
 }
 

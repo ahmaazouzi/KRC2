384fb7d6c22fa6b496786c1c2691c95a5f2147f4
Ahmed Maazouzi <ahmaazouzi@mix.wvu.edu>
2018-10-01 05:12:25 -0400

    Needs more work

--- ch2/2_03.c
+++ ch2/2_03.c
@@ -1,9 +1,43 @@
+/* This looks very very bad */
 #include "stdio.h"
 
+int atoi(char s[]);
 int main(){
-	int a;
-	float baba = 3.9444;
-	a = baba;
-	printf("%d\n", a);
+	printf("%d\n", atoi("FF"));
 	return 0;
+}
+
+int atoi(char s[]){
+	int n, i, z, x;
+	// x = 0;
+
+	// if (s[0] == '0' && (s[1] == 'x' || s[1] == 'X')){
+	// 	x = 2;
+	// }
+
+	n = 0;
+	for (i = 0; (s[i] >= '0' && s[i] <= '9') ||
+		(s[i] >= 'A' && s[i] <= 'F') ||
+		(s[i] >= 'a' && s[i] <= 'f')
+		; ++i) 
+	{
+		if (s[i] >= '0' && s[i] <= '9')
+			z = (s[i] - '0');
+		else if (s[i] >= 'A' && s[i] <= 'F'){
+			if (s[i] == 'A' || s[i] == 'a')
+				z = 10;
+			if (s[i] == 'B' || s[i] == 'b')
+				z = 11;
+			if (s[i] == 'C' || s[i] == 'c')
+				z = 12;
+			if (s[i] == 'D' || s[i] == 'd')
+				z = 13;
+			if (s[i] == 'E' || s[i] == 'e')
+				z = 14;
+			if (s[i] == 'F' || s[i] == 'f')
+				z = 15;
+		}
+		n = 16 * n + z;
+	}
+	return n;
 }

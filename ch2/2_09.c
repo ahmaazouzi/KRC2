17e6a2b919b2e188eef8b64fba0bea5ea2bdc1e1
Ahmed Maazouzi <ahmaazouzi@mix.wvu.edu>
2018-10-05 02:22:19 -0400

    Another slightly vague question. Need to spend time with bit crap

--- ch2/2_09.c
+++ ch2/2_09.c
@@ -1,5 +1,23 @@
+/* Not sure if this is faster. The conditional is removed
+ * and the the observation is made use of. I believe the answer
+ * is right.
+ */
+
 #include <stdio.h>
 
+int bitcount(unsigned x);
 int main(){
+	printf("%d\n", bitcount(15));
 	return 0;
+}
+
+int bitcount(unsigned x){
+	int b = 0;
+
+	while (x){
+		x &= (x - 1);
+		b++;
+	} 
+
+	return b;
 }

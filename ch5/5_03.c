6e671456d8881aaf750fa30b3aa73887f36c2cc0
Ahmed Maazouzi <ahmaazouzi@mix.wvu.edu>
2018-10-22 12:59:43 -0400

    Da

---
+++ ch5/5_03.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#define ALLOCSIZE 10000
+
+int main(){
+	return 0;
+}
+
+static char allocbuf[ALLOCSIZE];
+static char *allocp = allocbuf;
+
+char *alloc(int n){
+	if (allocbuf + ALLOCSIZE - allocp >= n){
+		allocp += n;
+		return allocp - n;
+	} else
+		return 0;
+}
+
+void afree(char *p){
+	if (p >= allocbuf && p < allocbuf + ALLOCSIZE)
+		allocp = p;
+}
+

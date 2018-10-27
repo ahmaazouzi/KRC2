b95c387a17057e7c37a827cc89bdd7ae7c212932
Ahmed Maazouzi <ahmaazouzi@mix.wvu.edu>
2018-10-27 03:40:55 -0400

    Still not working

--- ch5/5_10.c
+++ ch5/5_10.c
@@ -1,54 +1,64 @@
 #include <stdio.h>
 #include <string.h>
+#include <ctype.h>
+
 #define MAXLINE 1000
 
-int get_line(char *line, int max);
 
 int main(int argc, char *argv[]){
+	char *first, *second, *third, *temp;
+	int numnum;
 
-	char line[MAXLINE];
-	long lineno = 0;
-	int c, except = 0, number = 0, found = 0;
+	char *opstack[100];
+	int opindex = 0;
 
-	while (--argc > 0 && (*++argv)[0] == '-')
-		while ((c = *++argv[0]))
-			switch(c) {
-			case 'x':
-				except = 1;
-				break;
-			case('n'):
-				number = 1;
-				break;
-			default:
-				printf("find: illegal option %c\n", c);
-				argc = 0;
-				found = -1;
-				break;
+	while ((--argc > 0) && *++argv != NULL) 
+			if ((*argv)[0] == '+' || (*argv)[0] == '-'){
+				second = *argv;
+				temp = *argv;
+				while (isdigit((*--argv)[0]) || (*argv)[0] == 'z'){
+					if (isdigit((*argv)[0])){ 
+						if (numnum == 0){
+							first = *argv;
+							*argv = "z";
+						}
+						if (numnum == 1){
+							third = *argv;
+							opstack[opindex++] = first;
+							opstack[opindex++] = second;
+							opstack[opindex++] = third;
+							*argv = "z";
+						}
+						numnum++;
+					}
+				}
+				numnum = 0;
+				
+				*argv = temp;
+				*argv = "z";
 			}
-	if (argc != 1)
-		printf("Usage: find -x -n pattern\n");
-	else
-		while (get_line(line, MAXLINE) > 0) {
-			lineno++;
-			if ((strstr(line, *argv) != NULL) != except) {
-				if (number)
-					printf("%ld:", lineno);
-				printf("%s", line);
-				found++;
-			}
-		}
-	return found;
-}
 
-int get_line(char *s, int lim){
-	int c, i;
 
-	for (i=0; i<lim-1 && (c=getchar())!=EOF && c!='\n'; ++i)
-		s[i] = c;
-	if (c == '\n'){
-		s[i] = c;
-		++i;
+	for (int i = 0; i < opindex; i++){
+		printf("%s\n", opstack[i]);
 	}
-  	s[i] = '\0';
-  	return i;
+
+
+
+		// while (--argc > 0)
+		// if ((*++argv)[1] == '*')
+		// 	printf("WTF\n");
+	// if (argc != 1)
+	// 	printf("Usage: find -x -n pattern\n");
+	// else
+	// 	while (get_line(line, MAXLINE) > 0) {
+	// 		lineno++;
+	// 		if ((strstr(line, *argv) != NULL) != except) {
+	// 			if (number)
+	// 				printf("%ld:", lineno);
+	// 			printf("%s", line);
+	// 			found++;
+	// 		}
+	// 	}
+	// return found;
 }

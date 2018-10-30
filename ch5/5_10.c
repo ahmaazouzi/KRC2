5f8dd2b73b308f214e6e03240782d13179d1f45d
Ahmed Maazouzi <ahmaazouzi@mix.wvu.edu>
2018-10-30 02:45:54 -0400

    F finally

--- ch5/5_10.c
+++ ch5/5_10.c
@@ -1,22 +1,24 @@
 #include <stdio.h>
 #include <string.h>
 #include <ctype.h>
+#include <stdlib.h>
 
 #define MAXLINE 1000
 #define NONNUMBER "b"
 
-void expr(char *[], int, int);
+void expr(char *[], int);
 int main(int argc, char *argv[]){
 	int numnum = 0;
+	char *nums_ops[100]; 
 
 	for (int i = 1; i < argc; i++){
 		if (argv[i][0] == '+' || argv[i][0] == '*' || 
 		 argv[i][0] == '/' ||  argv[i][0] == '-' || isdigit(argv[i][0] )){
-			numnum++;
+			nums_ops[numnum++] = argv[i];
 		}
 	}
 
-	expr(argv, argc, numnum);
+	expr(nums_ops, numnum);
 
 
 	// int numnum, ind, operations;
@@ -84,41 +86,106 @@ int main(int argc, char *argv[]){
 
 }
 
-char *nums[100];
-char buff[100] = "";
 
-void expr(char *argv[], int argc, int opnum){
-	int numsonly;
-	for (int i = 1; i < argc; i++){
+
+int recursions = 0;
+
+
+void expr(char *argv[], int argc){
+	int tempnums, numsonly;
+	char *nums[100];
+	char *temp[100];
+	char buff[100] = "";
+
+	// tempnums = 0;
+	// for (int i = 0; i < argc; i++){
+	// 	temp[i] = argv[i];
+	// 	tempnums++;
+	// }
+	// numsonly = tempnums;
+
+	// printf("\n");
+
+	// // while (numsonly >= 1){
+	// 	for (int i = 0; i < numsonly; ++i){
+	// 		if (temp[i][0] == '+' || temp[i][0] == '*' || 
+	// 		 temp[i][0] == '/' ||  temp[i][0] == '-'){
+
+	// 		 	if (i > 1){
+	// 		 		strcat(buff, temp[i-1]);
+	// 		 		temp[i-1] = NONNUMBER;
+	// 		 		strcat(buff, temp[i]);
+	// 		 		// break;
+	// 		 	}
+	// 		 	if (i > 2){
+	// 		 		strcat(buff, temp[i-2]);
+	// 		 		temp[i-2] = NONNUMBER;
+	// 		 	}
+
+	// 		 	strcpy((temp[i] = malloc(sizeof(100))), buff);
+	// 		 	strcpy(buff, "");
+	// 		 	break;
+	// 		}
+	// 	}
+	// // }
+	// 	for (int i = 0; i < numsonly; i++){
+	// 		if (temp[i][0] == '+' || temp[i][0] == '*' || 
+	// 		 temp[i][0] == '/' ||  temp[i][0] == '-' || isdigit(temp[i][0] )){
+	// 			nums[numsonly++] = temp[i];
+	// 		}
+	// 	}
+
+	// 	for (int i = 0; i < count; ++i)
+	// 	{
+	// 		/* code */
+	// 	}
+
+	// 	return;
+
+	int rec = 0;
+	for (int i = 0; i < argc; i++){
 		if (argv[i][0] == '+' || argv[i][0] == '*' || 
 		 argv[i][0] == '/' ||  argv[i][0] == '-'){
-		 	if (i > 1){
+		 	if (i > 0){
 		 		strcat(buff, argv[i-1]);
 		 		argv[i-1] = NONNUMBER;
 		 		strcat(buff, argv[i]);
 		 	}
-		 	if (i > 2){
+		 	if (i > 1){
 		 		strcat(buff, argv[i-2]);
 		 		argv[i-2] = NONNUMBER;
 		 	}
-		 	argv[i] = buff;
-		 }
+		 	strcpy((argv[i] = malloc(sizeof(100))) ,buff); //argv[i] = buff;
+		 	strcpy(buff, "");
+		 	break;
+		}
 	}
+	printf("%s\n", buff);
+	
 
 	numsonly = 0;
 
-	for (int i = 1; i < argc; i++){
+	for (int i = 0; i < argc; i++){
 		if (argv[i][0] == '+' || argv[i][0] == '*' || 
 		 argv[i][0] == '/' ||  argv[i][0] == '-' || isdigit(argv[i][0] )){
 			nums[numsonly++] = argv[i];
 		}
 	}
 
+	for (int i = 0; i < numsonly; ++i)
+	{
+		printf("%s", nums[i]);
+	}
+
+	printf("\n");
+	// return;
+
 	if (numsonly == 1)
 		printf("%s\n", buff);
 	else {
-		strcpy(buff, "");
-		expr(nums, opnum, numsonly);
+		rec++;
+		printf("num rec: %d\n", numsonly);
+		expr(nums, numsonly);
 	}
 
 

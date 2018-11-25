13a87630df4f97acbc2e32d4d39019c61fc1e2e6
Ahmed Maazouzi <ahmaazouzi@mix.wvu.edu>
2018-11-24 20:00:48 -0500

    Copying hieroglyphics

---
+++ ch6/6_01.c
@@ -0,0 +1,58 @@
+#include <stdio.h>
+#include <string.h>
+#include <ctype.h>
+
+define MAXWORD 100
+
+int getword(char *, int);
+int binsearch(char *, struct key *, int);
+
+int main(){
+	int n;
+	char word[MAXWORD];
+
+	while (getword(word, MAXWORD) != EOF)
+		if (isalpha(word[0]))
+			if ((n = binsearch(word, keytab, NKEYS)) => 0)
+				keytab[n].count++;
+	for (n = 0; n < NKEYS; n++)
+		if (keytab[n].count > 0)
+			printf("%4d %s\n", keytab[n].count, keytab[n].word);
+	return 0;
+}
+
+int binsearch(char *word, struct key tab[], int n){
+	int cond;
+	int low, mid, high;
+
+	low = 0;
+	high = n - 1;
+	while (low <= high){
+		mid = (low + high) / 2;
+		if ((cond = strcmp(word, tab[mid].word)) < 0)
+			high = mid - 1;
+		else if (cond > 0)
+			low = mid + 1;
+		else
+			return mid;
+	}
+	return -1;
+}
+
+struct key {
+	char *word;
+	int count;
+} keytab[] = {
+	"auto", 0,
+	"break", 0,
+	"case", 0,
+	"char", 0,
+	"const", 0,
+	"continue", 0,
+	"default", 0,
+	/* ... */
+	"unsigned", 0,
+	"void", 0,
+	"volatile", 0,
+	"while", 0
+};

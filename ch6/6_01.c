1aac852e458f9018ea930e6a4d8d99d9cad02ccf
Ahmed Maazouzi <ahmaazouzi@mix.wvu.edu>
2018-11-25 00:41:00 -0500

    More copying

--- ch6/6_01.c
+++ ch6/6_01.c
@@ -2,7 +2,25 @@
 #include <string.h>
 #include <ctype.h>
 
-define MAXWORD 100
+#define NKEYS (sizeof keytab / sizeof(struct key))
+#define MAXWORD 100
+
+struct key {
+	char *word;
+	int count;
+} keytab[] = {
+	"auto", 0,
+	"break", 0,
+	"case", 0,
+	"char", 0,
+	"const", 0,
+	"continue", 0,
+	"default", 0,
+	"unsigned", 0,
+	"void", 0,
+	"volatile", 0,
+	"while", 0
+};
 
 int getword(char *, int);
 int binsearch(char *, struct key *, int);
@@ -13,7 +31,7 @@ int main(){
 
 	while (getword(word, MAXWORD) != EOF)
 		if (isalpha(word[0]))
-			if ((n = binsearch(word, keytab, NKEYS)) => 0)
+			if ((n = binsearch(word, keytab, NKEYS)) >= 0)
 				keytab[n].count++;
 	for (n = 0; n < NKEYS; n++)
 		if (keytab[n].count > 0)
@@ -39,20 +57,40 @@ int binsearch(char *word, struct key tab[], int n){
 	return -1;
 }
 
-struct key {
-	char *word;
-	int count;
-} keytab[] = {
-	"auto", 0,
-	"break", 0,
-	"case", 0,
-	"char", 0,
-	"const", 0,
-	"continue", 0,
-	"default", 0,
-	/* ... */
-	"unsigned", 0,
-	"void", 0,
-	"volatile", 0,
-	"while", 0
-};
+int getword(char *word, int lim){
+	int c, getch(void);
+	void ungetch(int);
+	char *w = word;
+
+	while (isspace(c = getch()))
+		;
+	if (c != EOF)
+		*w++ = c;
+	if (!isalpha(c)){
+		*w = '\0';
+		return 0;
+	}
+	for (; --lim > 0; w++)
+		if (!isalnum(*w = getch())){
+			ungetch(*w);
+			break;
+		}
+	*w = '\0';
+	return word[0];
+}
+
+#define BUFFSIZE 100
+
+char buf[BUFFSIZE];
+int bufp = 0;
+
+int getch(void){
+	return (bufp > 0) ? buf[--bufp]: getchar();
+}
+
+void ungetch(int c){  
+	if (bufp >= BUFFSIZE)
+		printf("ungetch: too many characters\n");
+	else
+		buf[bufp++] = c;
+}

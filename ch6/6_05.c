0d0d04ae4d4d0426459d1450aade103e9ad4cc56
Ahmed Maazouzi <ahmaazouzi@mix.wvu.edu>
2018-12-01 23:19:20 -0500

    TFAW

--- ch6/6_05.c
+++ ch6/6_05.c
@@ -1,104 +1,83 @@
-#include <stdio.h>
+#include "stdio.h"
+#include <stdlib.h>
 #include <string.h>
-#include <ctype.h>
 
-#define MAXWORD 100
-
-struct tnode {
-	char *word;
-	int count;
-	struct tnode *left;
-	struct tnode *right;
+struct nlist {
+	struct nlist *next;
+	char *name;
+	char *defn;
 };
 
-struct tnode *addtree(struct tnode *, char *);
-void treeprint(struct tnode *);
-int getword(char *, int);
+#define HASHSIZE 101
 
-int main(int argc, char const *argv[]){
-	struct tnode *root;
-	char word[MAXWORD];
+static struct nlist *hashtab[HASHSIZE];
 
-	root = NULL;
-	while (getword(word, MAXWORD) != EOF)
-		if (isalpha(word[0]))
-			root = addtree(root, word);
-	treeprint(root);
+unsigned hash(char *);
+struct nlist *lookup(char *);
+struct nlist *install(char *, char *);
+void undef(char *);
+
+int main(){
+	install("aa", "15000");
+	
+	undef("aa");
+	struct nlist *baba = lookup("aa");
+
+	// printf("rat: %d\n", hash("rat"));
+	// printf("tar: %d\n", hash("rat"));
+	
+	if (baba)
+		printf("%s\n", baba->defn);
+	else
+		printf("WTF, dude!!\n");
 	return 0;
 }
 
-#define INCOMMENT  1
-#define OUTCOMMENT 0
+unsigned hash(char *s){
+	unsigned hashval;
 
-int getword(char *word, int lim){
-	int c, getch(void), commentstate;
-	void ungetch(int);
-	char *w = word;
-	commentstate = OUTCOMMENT;
-
-	while (isspace(c = getch()) && c != '"' && c != '_')
-		;
-	if (c == '/' && commentstate == OUTCOMMENT)
-		if (getch() == '*'){
-			commentstate = INCOMMENT;
-			while ((c = getch())!= EOF){
-				if (c == '*')
-					if (getch() == '/'){
-						break;
-						commentstate = OUTCOMMENT;	
-					}
-			}
-		}
-	if (c == '"')
-		while ((c = getch())!= EOF && c != '"')
-			;
-	if (c != EOF)
-		*w++ = c;
-	if (!isalpha(c) && c != '"'){
-		*w = '\0';
-		return c;
-	}
-	for (; --lim > 0; w++)
-		if (!isalnum(*w = getch()) && *w != '_'){
-			ungetch(*w);
-			break;
-		}
-	*w = '\0';
-	return word[0];
+	for (hashval = 0; *s != '\0'; s++)
+		hashval = *s + 31 * hashval;
+	return hashval % HASHSIZE;
 }
 
-struct tnode *talloc(void);
+struct nlist *lookup(char *s){
+	struct nlist *np;
+
+	for (np = hashtab[hash(s)]; np != NULL; np = np->next)
+		if (strcmp(s, np->name) == 0)
+			return np;
+	return NULL;
+
+}
+
 char *strdupa(char *);
 
-struct tnode *addtree(struct tnode *p, char *w){
-	int cond;
+struct nlist *install(char *name, char *defn){
+	struct nlist *np;
+	unsigned hashval;
 
-	if (p == NULL){
-		p = talloc();
-		p->word = strdup(w);
-		p->count = 1;
-		p->left = p->right = NULL;
-	} else if ((cond = strcmp(w, p->word)) == 0)
-		p->count++;
-	else if (cond < 0)
-		p->left = addtree(p->left, w);
-	else
-		p->right = addtree(p->right, w);
-	return p;
+	if ((np = lookup(name)) == NULL){
+		np = (struct nlist *) malloc(sizeof(*np));
+		if (np == NULL || (np->name = strdupa(name)) == NULL)
+			return NULL;
+		hashval = hash(name);
+		np->next = hashtab[hashval];
+		hashtab[hashval] = np;
+	} else 
+		free((void *) np-> defn);
+	if ((np->defn = strdupa(defn)) == NULL)
+		return NULL;
+	return np;
 }
 
-void treeprint(struct tnode *p){
-	if (p != NULL){
-		treeprint(p->left);
-		printf("%4d %s\n", p->count, p->word);
-		treeprint(p->right);
+void undef(char *name){
+	unsigned hashval;
+	struct nlist *np;
+
+	if ((np = lookup(name)) != NULL){
+			np->next = NULL;
 	}
-}
-
-#include <stdlib.h>
-
-struct tnode *talloc(void){
-	return (struct tnode *) malloc(sizeof(struct tnode));
 }
 
 char *strdupa(char *s){
@@ -110,18 +89,5 @@ char *strdupa(char *s){
 	return p;
 }
 
-#define BUFFSIZE 100
 
-char buf[BUFFSIZE];
-int bufp = 0;
 
-int getch(void){
-	return (bufp > 0) ? buf[--bufp]: getchar();
-}
-
-void ungetch(int c){  
-	if (bufp >= BUFFSIZE)
-		printf("ungetch: too many characters\n");
-	else
-		buf[bufp++] = c;
-}

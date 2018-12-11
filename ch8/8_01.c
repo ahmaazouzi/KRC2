72a6fcffd5a332e717d7544262445c980b0e5a76
Ahmed Maazouzi <ahmaazouzi@mix.wvu.edu>
2018-12-10 22:37:13 -0500

    Done with 8.1

--- ch8/8_01.c
+++ ch8/8_01.c
@@ -1,32 +1,48 @@
 #include <unistd.h>
 #include <fcntl.h>
 #include <stdio.h>
+#include <stdarg.h>
+#include <stdlib.h>
 
 #define PERMS 666
+#define STANDARS_IN  0
+#define STANDARS_OUT 1
 
-int main(int argc, char const *argv[]){
-	void filecat(int, int);
+void error(char *, ...);
+
+int main(int argc, char *argv[]){
+	void filecat(int);
 	int f1;
 
 	if (argc == 1)
-		filecat(0, 1);
+		filecat(STANDARS_IN);
 	else
 		while (--argc > 0)
 			if ((f1 = open(*++argv, O_RDONLY, 0)) == -1){
-				printf("cat: can't open %s\n", *argv);
+				error("cat: can't open %s\n", *argv);
 			} else {
-				filecat(f1, 1);
+				filecat(f1);
 				close(f1);
 			}
 	return 0;
 }
 
-void filecat(int f1, int f2){
+void filecat(int f1){
 	char buf[BUFSIZ];
 	int n;
 
 	while ((n = read(f1, buf, BUFSIZ)) > 0)
-		if (write(1, buf, n) != n)
-			// error("cat: write error on some file");
-			printf("cat: write error on some file");
+		if (write(STANDARS_OUT, buf, n) != n)
+			error("cat: write error on stdout");
+}
+
+void error(char *fmt, ...){
+	va_list args;
+
+	va_start(args, fmt);
+	fprintf(stderr, "error, ");
+	vfprintf(stderr, fmt, args);
+	fprintf(stderr, "\n");
+	va_end(args);
+	exit(1);
 }
